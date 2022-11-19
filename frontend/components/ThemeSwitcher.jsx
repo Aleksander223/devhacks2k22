328e5074261890d22dbf0b0038fb9793811a40b7
Aleks <alexandruvlad.adam@gmail.com>
2022-11-19 14:12:48 +0200

    Fixed app init

--- frontend/components/ThemeSwitcher.jsx
+++ frontend/components/ThemeSwitcher.jsx
@@ -1,11 +1,16 @@
 import { useHookstate } from "@hookstate/core";
 import React from "react";
 import { themeState } from "../pages/_app";
+import { FiMoon } from "react-icons/fi";
 
 export default function ThemeSwitcher() {
     const theme = useHookstate(themeState);
 
-
-
-    return <p>Theme switch</p>
+    return (
+        <>
+            <button className="btn btn-square btn-accent">
+                <FiMoon/>
+            </button>
+        </>
+    );
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,9 +3,7 @@ import Navbar from '../components/Navbar';
 import { hookstate, useHookstate } from '@hookstate/core';
 import { localstored } from '@hookstate/localstored';
 
-export const themeState = hookstate('light', localstored({
-  key: 'theme'
-}));
+export const themeState = hookstate('light');
 
 function MyApp({ Component, pageProps }) {
   return (
