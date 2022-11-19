c182e45d1fb5c5850ffad79811ab8520e797ecdb
Paul <paulconst7000@gmail.com>
2022-11-19 17:59:20 +0200

    checkPaul3

--- frontend/components/Milestones.jsx
+++ frontend/components/Milestones.jsx
@@ -1,10 +1,16 @@
 
 
-const Milestones = () => {
+const Milestones = ({percent, text, title}) => {
     return(
         <>
+        <div className="card xl:card-side bg-base-100 shadow-md max-h-52 xl:max-h-32 border-neutral-base-200 border-2">
+        <progress className="progress progress-primary mx-auto mt-4 xl:mt-5 xl:mx-3 text-primary h-4" value= {percent} max="100"></progress>
+        <div className="p-4 xl:card-body">
+          <p className="font-bold text-primary text-center xl:text-left">{title}</p>
+          <p className="text-gray-400 text-center xl:text-left">{text}</p>
+        </div>
+      </div>
         
-        <progress className="progress progress-primary w-56" value="0" max="100"></progress>
         </>
     );
 }
---
+++ frontend/pages/Test.jsx
@@ -0,0 +1,15 @@
+import Milestones from "../components/Milestones";
+import KPICard from "../components/KPICard";
+
+const Test = () => {
+    return(
+        <>
+        <Milestones percent={50} title={"Protector of the forest lv 4"} text={"You have succesfully planted over 40 trees over the last cumunity sprint"} />
+
+        {/* <KPICard percent={50} title={"Protector of the forest lv 4"} text={"You have succesfully planted over 40 trees over the last cumunity sprint"} /> */}
+
+        </>
+    );
+}
+
+export default Test;
