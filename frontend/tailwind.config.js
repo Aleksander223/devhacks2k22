92f9fd31050c61497c7466eae797a8112290c3c8
Aleks <alexandruvlad.adam@gmail.com>
2022-11-20 10:33:44 +0200

    Added theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark"],
+    themes: [{
+      light: {
+        "primary": "#8518B7",
+        "secondary": "#A8E773",
+        "accent": "#5D0084",
+        "neutral": "#3D4451",
+        "base-100": "#FFFFFF",
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
+      },
+    }, "dark"],
     darkTheme: "dark"
   },
   plugins: [require("daisyui")]
